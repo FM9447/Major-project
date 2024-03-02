666234866e90ed717d927afc2da7e28c88dae528
Nixon T X <nixontx321@gmail.com>
2024-03-02 19:20:55 +0530

    shop commited

--- lib/dilshad/features/authentication/screens/login/login.dart
+++
@@ -1,6 +0,0 @@
-/*
-import 'package:t_store/features/authentication/screens/login/login.dart';
-
-SizedBox(width: double.infinity, child: ElevatedButton(onPressed:() => Get.to(() =>NavigationMenu()),child:const Text(TTexts.signIn)),
-const SizedBox(height: TSizes.spoceBtwItems),
-*/
